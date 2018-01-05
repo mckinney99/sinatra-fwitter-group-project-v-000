a9ff1dab67f0075a4c7db07f0e4d69340ae2e4e1
Eric McKinney <mckinney99@gmail.com>
2018-01-05 20:50:38 +0000

    automatically backed up by learn

---
+++ app/controllers/tweet_controller.rb
@@ -0,0 +1,69 @@
+class TweetController < ApplicationController
+
+  get "/tweets" do
+    if logged_in?
+      @user = current_user
+      @tweets = Tweet.all
+      erb :'tweets/tweets'
+    else
+      redirect to '/users/login'
+    end
+  end
+
+  get "/tweets/new" do
+    if logged_in?
+      erb :'tweets/create_tweet'
+    else
+      redirect to '/login'
+    end
+  end
+
+  post "/tweets" do
+    if params[:content].empty?
+      redirect 'tweets/new'
+    else
+      @tweet = Tweet.create(content: params[:content])
+      @tweet.user_id = current_user.id
+      @tweet.save
+    end
+  end
+
+  get "/tweets/:id" do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'tweets/show_tweet'
+    else
+      redirect '/login'
+    end
+  end
+
+  get "/tweets/:id/edit" do
+      @tweet = Tweet.find_by_id(params[:id])
+      if logged_in?
+        erb :'tweets/edit_tweet'
+      else
+      redirect '/login'
+    end
+  end
+
+  patch "/tweets/:id" do
+    @tweet = Tweet.find_by_id(params[:id])
+    if !params[:content].empty?
+      @tweet.content = params[:content]
+      @tweet.save
+      erb :'tweets/show_tweet'
+    else
+      redirect "/tweets/#{@tweet.id}/edit"
+    end
+  end
+
+  delete "/tweets/:id/delete" do
+    @tweet = Tweet.find_by_id(params[:id])
+    if logged_in? && @tweet.user_id == current_user.id
+      @tweet.delete
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+  end
+end
